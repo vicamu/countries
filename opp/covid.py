b546d7cbd39557aa4314a24c308ae2a6d132a9fb
Vicente Calatayud Muñoz <vicentecalatayudmunoz@MacBook-Pro-de-Vicente.local>
2021-07-14 21:57:10 +0200

    Clase objetos

--- opp/covid.py
+++ opp/covid.py
@@ -1,6 +1,7 @@
 import requests as req
 import json
 import funcovid
+import time
 
 
 # Traigo la información de internet y escribo el json
@@ -26,21 +27,29 @@ print(len(mun1))
 fecha1 = "2020/02/26 09:00:00"
 mun26feb = funcovid.get_by_date(fecha1, data)
 cases_26feb = funcovid.get_total_cases(mun26feb)
-print(f"Los casos totales a {fecha1.split(' ')} son: {cases_26feb} ")
+print(f"Los casos totales a {fecha1} son: {cases_26feb} ")
 
 # confirmados totales a 1-JUL-2020
 
 fecha2 = "2020/07/01 09:00:00"
 mun1jul = funcovid.get_by_date(fecha2, data)
 cases_1jul = funcovid.get_total_cases(mun1jul)
-print(f"Los casos totales a {fecha1.split(' ')} son: {cases_1jul} ")
+print(f"Los casos totales a {fecha2} son: {cases_1jul} ")
 
 # Obtener los 10 municipios con mayor cantidad de confirmados totales
 
 rank = 10
-worst_mun = funcovid.get_worst_muns(mun1jul,data)
-print(worst_mun)
+mun1jul_ok = funcovid.get_key_total_cases(mun1jul)
+worst_mun = funcovid.get_worst_muns(mun1jul_ok)
+funcovid.print_pretty((worst_mun[0:rank]))
 
+# Crear una lista con la sumatoria de los casos confirmados totales por día
+
+start = time.perf_counter()
+funcovid.get_sum_by_date(data)
+finish = time.perf_counter()
+
+print(finish)
 
 
 
--- opp/funcovid.py
+++ opp/funcovid.py
@@ -1,3 +1,6 @@
+def print_pretty(data):
+    for i,mun in enumerate(data):
+        print(f"{i+1}. {mun['municipio_distrito']}: {mun['casos_confirmados_totales']}")
 
 def get_by_date(date,data):
     result = [] 
@@ -15,13 +18,45 @@ def get_total_cases(muns):
             result += 0
     return result
 
-def get_worst_muns(rank,data):
+def get_key_total_cases(data):
+    result = []
+    for mun in data:
+        try:
+            if type(mun["casos_confirmados_totales"]) == int:
+                result.append(mun)
+        except:
+            continue
+    return result
+  
 
+
+def get_worst_muns(data):
     try:
-        result = (sorted(data, key=lambda mun: mun["casos_confirmados_totales"], reverse=False))
+        result = sorted(data, key=lambda mun: mun["casos_confirmados_totales"], reverse=True)
     except KeyError:
         next 
     return result
 
+def get_sum_by_date(data):
+    count = 0
+    result = {}
+
+    fecha = "2020/03/02"
+    for mun in data:
+        control = True
+        if "2020/03/02" == fecha:
+            try:
+                count += mun["casos_confirmados_totales"]
+            except KeyError:
+                control = False
+        elif control:
+            result[fecha] = count
+            count = 0
+            fecha = "2020/03/02"
+            control = True
+    for k,v in result.items():
+        print(f"{k}: {v}")
+
+    print(len(result))
 
     
